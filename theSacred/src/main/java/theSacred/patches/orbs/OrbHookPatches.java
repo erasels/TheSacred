c918b5ade39b900b82ced9b1cf40b8d414ef08ec
erasels <emanuelbedburdick@yahoo.de>
2019-10-16 13:24:06 +0200

    Orb ReceiveDamageHook

--- theSacred/src/main/java/theSacred/patches/orbs/OrbHookPatches.java
+++ theSacred/src/main/java/theSacred/patches/orbs/OrbHookPatches.java
@@ -143,4 +143,44 @@ public class OrbHookPatches {
             }
         }
     }
+
+    @SpirePatch(clz = DamageInfo.class, method = "applyPowers")
+    public static class DamageInfoDamageReceiveHook {
+        @SpireInsertPatch(locator = Locator.class, localvars = {"tmp"})
+        public static void callHook(DamageInfo __instance, AbstractCreature owner, AbstractCreature target, @ByRef float[] tmp) {
+            for (AbstractOrb o : UC.p().orbs) {
+                if (o instanceof DamageAndBlockModifyOrb) {
+                    tmp[0] = ((DamageAndBlockModifyOrb) o).atPlayerDamageReceive(tmp[0], __instance.type);
+                }
+            }
+        }
+
+        private static class Locator extends SpireInsertLocator {
+            @Override
+            public int[] Locate(CtBehavior ctBehavior) throws Exception {
+                Matcher finalMatcher = new Matcher.FieldAccessMatcher(AbstractCreature.class, "powers");
+                return new int[]{LineFinder.findAllInOrder(ctBehavior, finalMatcher)[1]};
+            }
+        }
+    }
+
+    @SpirePatch(clz = AbstractMonster.class, method = "calculateDamage")
+    public static class AbstractMonsterDamageReceiveHook {
+        @SpireInsertPatch(locator = Locator.class, localvars = {"tmp"})
+        public static void callHook(AbstractMonster __instance, int dmg, @ByRef float[] tmp) {
+            for (AbstractOrb o : UC.p().orbs) {
+                if (o instanceof DamageAndBlockModifyOrb) {
+                    tmp[0] = ((DamageAndBlockModifyOrb) o).atPlayerDamageReceive(tmp[0], DamageInfo.DamageType.NORMAL);
+                }
+            }
+        }
+
+        private static class Locator extends SpireInsertLocator {
+            @Override
+            public int[] Locate(CtBehavior ctBehavior) throws Exception {
+                Matcher finalMatcher = new Matcher.FieldAccessMatcher(AbstractPlayer.class, "powers");
+                return LineFinder.findInOrder(ctBehavior, finalMatcher);
+            }
+        }
+    }
 }
