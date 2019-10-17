6aeb77c41cba58b06d6d31dd227e0d38c476f4e5
erasels <emanuelbedburdick@yahoo.de>
2019-10-17 19:22:08 +0200

    Card and power Brittle Barrier

---
+++ theSacred/src/main/java/theSacred/cards/common/BrittleBarrier.java
@@ -0,0 +1,44 @@
+package theSacred.cards.common;
+
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import theSacred.cards.abstracts.AlignedCard;
+import theSacred.cards.abstracts.SacredCard;
+import theSacred.powers.BrittleBarrierPower;
+import theSacred.util.CardInfo;
+
+import static theSacred.TheSacred.makeID;
+import static theSacred.util.UC.*;
+
+public class BrittleBarrier extends SacredCard implements AlignedCard {
+    private final static CardInfo cardInfo = new CardInfo(
+            "BrittleBarrier",
+            0,
+            CardType.SKILL,
+            CardTarget.SELF);
+
+    public final static String ID = makeID(cardInfo.cardName);
+
+    private static final int BLOCK = 4;
+    private static final int UPG_BLOCK = 3;
+
+    private static final int MAGIC = 1;
+
+    public BrittleBarrier() {
+        super(cardInfo, false);
+
+        setBlock(BLOCK, UPG_BLOCK);
+        setMagic(MAGIC);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        doDef(block);
+    }
+
+    @Override
+    public void alignEffect(AbstractCreature target) {
+        doPow(p(), new BrittleBarrierPower(magicNumber));
+    }
+}
---
+++ theSacred/src/main/java/theSacred/powers/BrittleBarrierPower.java
@@ -0,0 +1,72 @@
+package theSacred.powers;
+
+import basemod.interfaces.CloneablePowerInterface;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import com.megacrit.cardcrawl.powers.WeakPower;
+import theSacred.TheSacred;
+import theSacred.powers.abstracts.AbstractSacredPower;
+import theSacred.util.UC;
+
+import static theSacred.util.UC.doPow;
+import static theSacred.util.UC.p;
+
+public class BrittleBarrierPower extends AbstractSacredPower implements CloneablePowerInterface {
+    public static final String POWER_ID = TheSacred.makeID("BrittleBarrier");
+    private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    public static final String NAME = powerStrings.NAME;
+    public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+
+    public BrittleBarrierPower(int amount2, AbstractCreature owner) {
+        name = NAME;
+        ID = POWER_ID;
+        this.owner = owner;
+        this.amount = 1;
+        this.amount2 = amount2;
+        type = PowerType.BUFF;
+        updateDescription();
+        isTurnBased = true;
+        loadRegion("barricade");
+        isBarrierPower = true;
+    }
+
+    public BrittleBarrierPower(int amount2) {
+        this(amount2, p());
+    }
+
+    @Override
+    public void atStartOfTurn() {
+        UC.generalPowerLogic(this);
+    }
+
+    public int onAttacked(DamageInfo info, int damageAmount) {
+        if (info.owner != null && info.type != DamageInfo.DamageType.THORNS && info.type != DamageInfo.DamageType.HP_LOSS && info.owner != this.owner) {
+            flash();
+            doPow(info.owner, new WeakPower(info.owner, amount2, false));
+        }
+        return damageAmount;
+    }
+
+    @Override
+    public void stackPower(int stackAmount) {
+        this.fontScale = 8.0F;
+        this.amount2 += stackAmount;
+    }
+
+    //TODO: Add generate particle vfx that renders a barrier?
+
+    @Override
+    public void updateDescription() {
+        description = DESCRIPTIONS[0] + amount2 + DESCRIPTIONS[1];
+    }
+
+    @Override
+    public AbstractPower makeCopy() {
+        BrittleBarrierPower tmp = new BrittleBarrierPower(amount2, owner);
+        tmp.amount = this.amount;
+        return tmp;
+    }
+}
