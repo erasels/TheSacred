6fe0666ad12780f8b69d5a17860853bad5cc9442
erasels <emanuelbedburdick@yahoo.de>
2019-10-18 15:27:54 +0200

    Card Persuasion Needles

---
+++ theSacred/src/main/java/theSacred/cards/common/PersuasionNeedles.java
@@ -0,0 +1,55 @@
+package theSacred.cards.common;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInDiscardAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import theSacred.cards.abstracts.AlignedCard;
+import theSacred.cards.abstracts.SacredCard;
+import theSacred.util.CardInfo;
+
+import static theSacred.TheSacred.makeID;
+import static theSacred.util.UC.*;
+
+public class PersuasionNeedles extends SacredCard implements AlignedCard {
+    private final static CardInfo cardInfo = new CardInfo(
+            "PersuasionNeedles",
+            0,
+            CardType.ATTACK,
+            CardTarget.ENEMY);
+
+    public final static String ID = makeID(cardInfo.cardName);
+
+    private static final int DAMAGE = 2;
+    private static final int UPG_DAMAGE = 1;
+
+    private static final int MAGIC = 3;
+
+    public PersuasionNeedles() {
+        super(cardInfo, false);
+
+        setDamage(DAMAGE, UPG_DAMAGE);
+        setMagic(MAGIC);
+        setExhaust(true);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        //TODO:Add Vfx
+        for (int i = 0; i < magicNumber ; i++) {
+            doDmg(m, damage, DamageInfo.DamageType.NORMAL, AbstractGameAction.AttackEffect.NONE, true);
+        }
+    }
+
+    @Override
+    public void alignEffect(AbstractCreature target) {
+        atb(new MakeTempCardInDiscardAction(this.makeStatEquivalentCopy(), 1));
+    }
+
+    @Override
+    public float getTitleFontSize() {
+        return 18f;
+    }
+}
