e8707df44e6a36ae979b91a5b04d192df6718578
erasels <emanuelbedburdick@yahoo.de>
2019-10-21 13:48:07 +0200

    Added consumer to ClearDebuffsAction

--- theSacred/src/main/java/theSacred/actions/utility/ClearAllDebuffsAction.java
+++ theSacred/src/main/java/theSacred/actions/utility/ClearAllDebuffsAction.java
@@ -7,16 +7,27 @@ import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.powers.AbstractPower;
 import theSacred.util.UC;
 
+import java.util.function.Consumer;
+
 public class ClearAllDebuffsAction extends AbstractGameAction {
+    private Consumer<AbstractPower> callback;
 
     public ClearAllDebuffsAction(AbstractCreature target) {
+        this(target, null);
+    }
+
+    public ClearAllDebuffsAction(AbstractCreature target, Consumer<AbstractPower> callback) {
         this.target = target;
+        this.callback = callback;
     }
 
     public void update() {
         for (AbstractPower power : target.powers) {
             if (power.type == AbstractPower.PowerType.DEBUFF && !(power instanceof InvisiblePower)) {
                 UC.att(new RemoveSpecificPowerAction(target, target, power));
+                if(callback != null) {
+                    callback.accept(power);
+                }
             }
         }
         isDone = true;
