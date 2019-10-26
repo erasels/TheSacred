902f6b75cb4c7727bde9e2da229ae3ccf16b8157
erasels <emanuelbedburdick@yahoo.de>
2019-10-26 22:13:29 +0200

    Card Quickrift and action

---
+++ theSacred/src/main/java/theSacred/actions/unique/QuickRiftBlockAction.java
@@ -0,0 +1,23 @@
+package theSacred.actions.unique;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import theSacred.cards.common.QuickRift;
+import theSacred.util.UC;
+
+public class QuickRiftBlockAction extends AbstractGameAction {
+    private QuickRift c;
+    public QuickRiftBlockAction(QuickRift c) {
+        this.c = c;
+    }
+
+    @Override
+    public void update() {
+        c.baseBlock = c.blockHack;
+        c.applyPowersToBlock();
+
+        UC.doDef(c.block, true);
+        c.baseBlock = 0;
+        c.blockHack = 0;
+        isDone = true;
+    }
+}
