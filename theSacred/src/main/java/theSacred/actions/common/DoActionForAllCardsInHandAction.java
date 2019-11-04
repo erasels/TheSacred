8fe0f923f5a5126eec126909bf5bbab82bf49a2f
erasels <emanuelbedburdick@yahoo.de>
2019-11-05 00:53:20 +0100

    Update DoActionForAllCardsInHandAction.java

--- theSacred/src/main/java/theSacred/actions/common/DoActionForAllCardsInHandAction.java
+++ theSacred/src/main/java/theSacred/actions/common/DoActionForAllCardsInHandAction.java
@@ -8,15 +8,26 @@ import java.util.ArrayList;
 import java.util.function.Consumer;
 
 public class DoActionForAllCardsInHandAction extends AbstractGameAction {
-    private Consumer<ArrayList<AbstractCard>> callback;
+    private Consumer<ArrayList<AbstractCard>> callback1;
+    private Consumer<AbstractCard> callback2;
 
     public DoActionForAllCardsInHandAction(Consumer<ArrayList<AbstractCard>> callback) {
-        this.callback = callback;
+        this.callback1 = callback;
+    }
+
+    public DoActionForAllCardsInHandAction(int x, Consumer<AbstractCard> callback) {
+        this.callback2 = callback;
     }
 
     @Override
     public void update() {
-        callback.accept(UC.p().hand.group);
+        if(callback1 != null) {
+            callback1.accept(UC.p().hand.group);
+        } else {
+            for(AbstractCard c : UC.p().hand.group) {
+                callback2.accept(c);
+            }
+        }
         isDone = true;
     }
 }
