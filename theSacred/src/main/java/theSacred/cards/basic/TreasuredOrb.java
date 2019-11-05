9d406d5d46236f1d33fce5791a8f3219a6a7bd48
erasels <emanuelbedburdick@yahoo.de>
2019-11-05 14:01:27 +0100

    Added vfx TreasuredOrb

--- theSacred/src/main/java/theSacred/cards/basic/TreasuredOrb.java
+++ theSacred/src/main/java/theSacred/cards/basic/TreasuredOrb.java
@@ -1,5 +1,6 @@
 package theSacred.cards.basic;
 
+import com.badlogic.gdx.graphics.Color;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
@@ -8,6 +9,8 @@ import theSacred.actions.common.OrbCheckAction;
 import theSacred.cards.abstracts.SacredCard;
 import theSacred.orbs.YinYangOrb;
 import theSacred.util.CardInfo;
+import theSacred.vfx.general.ButtonConfirmedEffect;
+import theSacred.vfx.general.RunAnimationEffect;
 
 import static theSacred.TheSacred.makeID;
 import static theSacred.util.UC.*;
@@ -31,6 +34,8 @@ public class TreasuredOrb extends SacredCard {
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
+        doAnim(RunAnimationEffect.ANIS.SPELLA);
+        doVfx(new ButtonConfirmedEffect(m.hb.cX, m.hb.cY, Color.SALMON, 2f, 3f));
         doDmg(m, damage, AbstractGameAction.AttackEffect.BLUNT_LIGHT);
         channelYY();
         if(upgraded) {
