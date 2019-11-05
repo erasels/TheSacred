212fe0bbe1dd0ccb70465e861b6ffbc2211c1291
erasels <emanuelbedburdick@yahoo.de>
2019-11-05 20:35:34 +0100

    Animation SpellB

--- theSacred/src/main/java/theSacred/vfx/general/RunAnimationEffect.java
+++ theSacred/src/main/java/theSacred/vfx/general/RunAnimationEffect.java
@@ -7,7 +7,7 @@ import theSacred.TheSacred;
 public class RunAnimationEffect extends AbstractGameEffect {
     private String key;
     public enum ANIS {
-       IDLE, GUARDA, GUARDB, HITLOW, HITHIGH, FAILURE, LOWKICK, GRAB, RODWHACK, RODSTAB, RODSLASH, BACKFLIP, BACKFLIPKICK, SPELLA, WINA, WINB, DOWNED, DOWNIDLE
+       IDLE, GUARDA, GUARDB, HITLOW, HITHIGH, FAILURE, LOWKICK, GRAB, RODWHACK, RODSTAB, RODSLASH, BACKFLIP, BACKFLIPKICK, SPELLA, SPELLB, WINA, WINB, DOWNED, DOWNIDLE
     }
 
     public RunAnimationEffect(String animation_key) {
@@ -57,6 +57,9 @@ public class RunAnimationEffect extends AbstractGameEffect {
                 break;
             case SPELLA:
                 key = "spellA";
+                break;
+            case SPELLB:
+                key = "spellB";
                 break;
             case WINA:
                 key = "winA";
