8ab496ac7214327189bcbdc735e7e555ddec8c9b
erasels <emanuelbedburdick@yahoo.de>
2020-11-16 14:36:15 +0100

    Utility isInCombat check

--- theSacred/src/main/java/theSacred/util/UC.java
+++ theSacred/src/main/java/theSacred/util/UC.java
@@ -281,7 +281,7 @@ public class UC {
     }
 
     public static boolean isInCombat() {
-        return CardCrawlGame.isInARun() && AbstractDungeon.getCurrRoom().phase != null && AbstractDungeon.getCurrRoom().phase == AbstractRoom.RoomPhase.COMBAT;
+        return CardCrawlGame.isInARun() && AbstractDungeon.currMapNode != null && AbstractDungeon.getCurrRoom().phase != null && AbstractDungeon.getCurrRoom().phase == AbstractRoom.RoomPhase.COMBAT;
     }
 
     public static <T> T getRandomItem(ArrayList<T> list) {
