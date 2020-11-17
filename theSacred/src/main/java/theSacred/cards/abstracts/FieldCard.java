fdc7ed87cd4c6272f2e208c62e697a4a717999f5
erasels <emanuelbedburdick@yahoo.de>
2020-11-17 20:45:50 +0100

    Added Field card top text and custom keyword, fixed ctor

--- theSacred/src/main/java/theSacred/cards/abstracts/FieldCard.java
+++ theSacred/src/main/java/theSacred/cards/abstracts/FieldCard.java
@@ -1,20 +1,42 @@
 package theSacred.cards.abstracts;
 
+import basemod.helpers.TooltipInfo;
+import com.badlogic.gdx.graphics.Color;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.UIStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import theSacred.TheSacred;
+import theSacred.characters.SacredCharacter;
 import theSacred.mechanics.field.FieldSystem;
 import theSacred.util.CardInfo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class FieldCard extends SacredCard {
+    public static final UIStrings uiStrings = CardCrawlGame.languagePack.getUIString(TheSacred.makeID("FieldCards"));
 
     public FieldCard(CardInfo cardInfo, boolean upgradesDescription) {
-        super(cardInfo, upgradesDescription);
+        this(SacredCharacter.Enums.COLOR_SACRED, cardInfo.cardName, cardInfo.cardCost, cardInfo.cardType, cardInfo.cardTarget, cardInfo.cardRarity, upgradesDescription);
     }
 
     public FieldCard(CardColor color, String cardName, int cost, CardType cardType, CardTarget target, CardRarity rarity, boolean upgradesDescription) {
         super(color, cardName, cost, cardType, target, rarity, upgradesDescription);
+        setTopText(uiStrings.TEXT[0], Color.ROYAL);
+    }
+
+    @Override
+    public List<TooltipInfo> getCustomTooltipsTop() {
+        ArrayList<TooltipInfo> tps = (ArrayList<TooltipInfo>) super.getCustomTooltipsTop();
+        if(tps == null) {
+            tps = new ArrayList<>();
+        }
+        if(!FieldSystem.fields.contains(this)) {
+            tps.add(0, new TooltipInfo(TheSacred.fieldKeywords[0], TheSacred.fieldKeywords[1]));
+        }
+        return tps;
     }
 
     @Override
